0043fdae9d8d1fe686526e561d29f5cecfa9d832
Oliver Beach <17b23772@marling.school>
2020-10-06 11:20:15 +0000

    Nearly Combined The two

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-print ("Welcome To The Vegetable Guessing Game")
+print ("Welcome To The Vegetable And Animal Guessing Game")
 print("\n")
-print ("The Choices Will Be : Carrot, Broccoli, Peas, Sweetcorn")
-print("\n")
-print("I Will Now Attempt To Guess Your Vegetable")
+print ("The Choices Will Be : Carrot, Broccoli, Peas, Sweetcorn. Y/N")
+answer = input().lower()
+if answer == "y":
+  print("I Will Now Attempt To Guess Your Vegetable")
 print("\n")
 print("Is Your Vegetable Green? Y/N")
 answer = input().lower()
@@ -21,3 +22,22 @@ else:
     print("Your Vegetable Is A Carrot!")
   else:
     print ("Your Vegetable Is A Sweetcorn!")
+
+print(" Or If Your Choosing An Animal: Lion, Ostrich, Whale. Y/N")
+answer = input().lower()
+else:
+  print("Pick either Ostrich, Lion or Whale")
+print("I will attempt to guess your choice")
+print("Does the animal live in the water? Y/N")
+answer = input().lower()
+
+if answer == "n":
+   print("Does the animal have wings? Y/N")
+   answer = input().lower()
+   if answer == "y":
+       print("It must be an Ostrich!")
+   else:
+       print("It must be a Lion!")
+else:
+   print("It must be a Whale!")
+
